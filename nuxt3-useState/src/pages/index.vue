b0ba86b6f6557f69b1332da82132969d17fff4a8
mugi0096 <ue.tsukasa.19980106@gmail.com>
2023-01-04 14:27:14 +0900

    ✨ add user state

---
+++ nuxt3-useState/src/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <nuxt-link to="/user">ユーザーページ</nuxt-link>
+  </div>
+</template>
---
+++ nuxt3-useState/src/pages/user.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { stringify } from 'querystring';
+
+const user = useState<{ id: string, name: string, mail: string }>('login-user', () => {
+  console.log('retrieving user info...');
+  return {
+    id: '123',
+    name: 'ueno',
+    mail: 'test@example.com'
+  };
+})
+</script>
+
+<template>
+  <div>
+    <h1>useState実装例</h1>
+    <nuxt-link to="/user-detail">{{ user.name }}さんの詳細ページ</nuxt-link>
+  </div>
+</template>
